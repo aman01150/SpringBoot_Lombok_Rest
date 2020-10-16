4374a643cba23c5a7b33ca0f298b8ddf4e9c6439
aman mishra <aman01150@gmail.com>
2020-10-16 20:17:27 +0530

    first commit

---
+++ src/main/java/aman/springframework/royalbeer/RoyalBeerApplication.java
@@ -0,0 +1,13 @@
+package aman.springframework.royalbeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyalBeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoyalBeerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aman/springframework/royalbeer/RoyalBeerApplicationTests.java
@@ -0,0 +1,13 @@
+package aman.springframework.royalbeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoyalBeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
